6b1bc1448df7cffcc52fc03ddc262bf89bba4df3
Manlek25 <144284833+Manlek25@users.noreply.github.com>
2025-06-04 14:50:50 +0000

    Criação no nível novato do jogo de batalha naval

--- batalhanaval.c
+++ batalhanaval.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+const int TAMANHO_TABULEIRO = 10;
+const int TAMANHO_NAVIO = 3;
+
+int main() {
+    // Inicialização direta com todos os valores 0
+    int tabuleiro[10][10] = { {0} };
+
+    // Define posições iniciais dos navios
+    int linhaNavioHorizontal = 2;
+    int colunaNavioHorizontal = 4;
+
+    int linhaNavioVertical = 5;
+    int colunaNavioVertical = 7;
+
+    // Posiciona navio horizontal
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linhaNavioHorizontal][colunaNavioHorizontal + i] = 3;
+    }
+
+    // Posiciona navio vertical
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linhaNavioVertical + i][colunaNavioVertical] = 3;
+    }
+
+    // Exibe o tabuleiro
+    printf("Tabuleiro de Batalha Naval:\n\n");
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
